a728989ffe9f1daf1664e7febdf3b4684f610607
Steve Gore <steve.j.gore@gmail.com>
2020-06-10 17:23:28 +1000

    Use start.sh to install git and repo

---
+++ apps.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "--- Installing formulae and casks ---"
+brew install $(cat brew/formulae)
+brew cask install $(cat brew/casks)
+
+# Terminal setup
+echo "--- Oh-my-zsh ---"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+echo "--- Powerline10k ---"
+echo 'source /usr/local/opt/powerlevel10k/powerlevel10k.zsh-theme' >>! ~/.zshrc
+
+echo "Open iTerm2 and run terminal.sh"
+
+# TODO:
+# install rust
+# install stuff from app store
+# add all licence keys to apps
+# configure forklift with webdav
+# configure omnifocus and enpass
+# configure vs code
+# sign into FireFox
--- start.sh
+++ start.sh
@@ -8,23 +8,5 @@ echo "--- Installing Homebrew ---"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 echo "--- Installing formulae and casks ---"
-brew install $(cat brew/formulae)
-brew cask install $(cat brew/casks)
-
-# Terminal setup
-echo "--- Oh-my-zsh ---"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-echo "--- Powerline10k ---"
-echo 'source /usr/local/opt/powerlevel10k/powerlevel10k.zsh-theme' >>! ~/.zshrc
-
-echo "Open iTerm2 and run terminal.sh"
-
-# TODO:
-# install rust
-# install stuff from app store
-# add all licence keys to apps
-# configure forklift with webdav
-# configure omnifocus and enpass
-# configure vs code
-# sign into FireFox
+brew install git
+git clone git@github.com:stevegore/setup.git
