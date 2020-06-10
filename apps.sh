aab3601f7c9d89ef0f27d5074078ffb9945afad4
Steve Gore <steve.j.gore@gmail.com>
2020-06-10 17:41:05 +1000

    Install Rust, tidy comments

--- apps.sh
+++ apps.sh
@@ -4,20 +4,18 @@ echo "--- Installing formulae and casks ---"
 brew install $(cat brew/formulae)
 brew cask install $(cat brew/casks)
 
-# Terminal setup
 echo "--- Oh-my-zsh ---"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 echo "--- Powerline10k ---"
 echo 'source /usr/local/opt/powerlevel10k/powerlevel10k.zsh-theme' >>! ~/.zshrc
 
+echo "--- Rust ---"
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
 echo "Open iTerm2 and run terminal.sh"
 
 # TODO:
-# install rust
-# install stuff from app store
-# add all licence keys to apps
-# configure forklift with webdav
-# configure omnifocus and enpass
-# configure vs code
-# sign into FireFox
+# Install purchases from macOS App Store
+# Add all licence keys to apps
+# Configure installed apps (VS Code, FireFox, Proxifier, OmniFocus, Enpass, ForkLift, etc.)
